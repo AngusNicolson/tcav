994c34c42f7ba4fd3971c3f20ef5a090c38d0eda
Angus Nicolson <angusjnicolson@gmail.com>
2021-10-12 10:31:59 +0100

    Add prefix to dataset

--- tcav/activation_generator.py
+++ tcav/activation_generator.py
@@ -30,7 +30,7 @@ else:
 
 class ActivationGenerator:
     """Activation generator for a basic image model"""
-    def __init__(self, model, source_json, acts_dir, dataset_class, max_examples=500, num_workers=0):
+    def __init__(self, model, source_json, acts_dir, dataset_class, max_examples=500, num_workers=0, prefix):
         """
         source_json (str): Path to a .json with filepaths for each img, categorised by concept
         dataset_class (Dataset): A dataset class to use in
@@ -38,6 +38,7 @@ class ActivationGenerator:
         self.model = model
         self.model.model.to(device)
         self.source_json = Path(source_json)
+        self.prefix = prefix
         self.acts_dir = Path(acts_dir)
         self.dataset_class = dataset_class
         self.max_examples = max_examples
@@ -72,7 +73,7 @@ class ActivationGenerator:
 
     def get_activations_for_concept(self, concept, bottleneck_names, batch_size=32, shuffle=True):
         """Get's activations for specified bottlenecks as np.arrays with no gradients"""
-        dataset = self.dataset_class(self.concept_dict, concept)
+        dataset = self.dataset_class(self.concept_dict, concept, prefix=self.prefix)
         self.model.eval()
         dataloader = DataLoader(dataset, batch_size, shuffle=shuffle, num_workers=self.num_workers)
         bns = {bn: [] for bn in bottleneck_names}
@@ -111,7 +112,7 @@ class ActivationGenerator:
     def get_examples_for_concept(self, concept, n=None, shuffle=False):
         if n is None:
             n = self.max_examples
-        dataset = self.dataset_class(self.concept_dict, concept)
+        dataset = self.dataset_class(self.concept_dict, concept, prefix=self.prefix)
         dataloader = DataLoader(dataset, n, shuffle=shuffle, num_workers=self.num_workers)
         for sample in dataloader:
             imgs = sample["img"]

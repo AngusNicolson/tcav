65579206f7623056409fc8a82e2c56ae8563aa0c
AngusNicolson <angusjnicolson@gmail.com>
2021-12-17 11:04:58 +0000

    Save intercept

--- tcav/cav.py
+++ tcav/cav.py
@@ -61,6 +61,8 @@ class CAV(object):
               save_dict['hparams'], save_dict['saved_path'])
     cav.accuracies = save_dict['accuracies']
     cav.cavs = save_dict['cavs']
+    if "intercept" in save_dict.keys():
+      cav.intercept = save_dict["intercept"]
     return cav
 
   @staticmethod
@@ -145,6 +147,7 @@ class CAV(object):
     self.bottleneck = bottleneck
     self.hparams = hparams
     self.save_path = save_path
+    self.intercept = None
 
   def train(self, acts):
     """Train the CAVs from the activations.
@@ -175,6 +178,7 @@ class CAV(object):
       # if there were only two labels, the concept is assigned to label 0 by
       # default. So we flip the coef_ to reflect this.
       self.cavs = [-1 * lm.coef_[0], lm.coef_[0]]
+      self.intercept = lm.intercept_[0]
     else:
       self.cavs = [c for c in lm.coef_]
     self._save_cavs()
@@ -220,6 +224,7 @@ class CAV(object):
         'hparams': self.hparams,
         'accuracies': self.accuracies,
         'cavs': self.cavs,
+        'intercept': self.intercept,
         'saved_path': self.save_path
     }
     if self.save_path is not None:

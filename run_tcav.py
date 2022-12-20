eb8fb7b3e709d618f2b69f2164f6a166ff4c65af
AngusNicolson <angusjnicolson@gmail.com>
2022-12-20 12:06:55 +0000

    black

--- run_tcav.py
+++ run_tcav.py
@@ -1,4 +1,3 @@
-
 from argparse import ArgumentParser
 from pathlib import Path
 import json
@@ -29,8 +28,8 @@ def main(args):
     overwrite = args.overwrite
 
     working_dir = Path(args.working_dir) / act_func
-    activation_dir = working_dir / 'activations'
-    cav_dir = working_dir / 'cavs'
+    activation_dir = working_dir / "activations"
+    cav_dir = working_dir / "cavs"
     grad_dir = working_dir / "grads"
 
     source_dir = Path(args.source_dir)  # '/home/lina3782/labs/explain/imagenet'
@@ -57,9 +56,16 @@ def main(args):
         json.dump(source_json, fp, indent=2)
 
     prefix = str(data_path) + "/"
-    act_generator = act_gen.ActivationGenerator(mymodel, source_json_path, activation_dir, JsonDataset,
-                                                max_examples=max_examples, prefix=prefix, num_workers=num_workers,
-                                                act_func=act_func)
+    act_generator = act_gen.ActivationGenerator(
+        mymodel,
+        source_json_path,
+        activation_dir,
+        JsonDataset,
+        max_examples=max_examples,
+        prefix=prefix,
+        num_workers=num_workers,
+        act_func=act_func,
+    )
 
     mytcav = tcav.TCAV(
         target,
@@ -70,16 +76,20 @@ def main(args):
         cav_dir=cav_dir,
         num_random_exp=num_random_exp,
         do_random_pairs=True,
-        grad_dir=grad_dir
+        grad_dir=grad_dir,
     )
 
     print("Training CAVs...")
+    print("This may take a while... Go get coffee!")
     mytcav.train_cavs(overwrite=overwrite)
+    print("Training complete!")
 
-    print('This may take a while... Go get coffee!')
+    print("Running TCAV...")
     results = mytcav.run(overwrite=overwrite)
 
-    fig = utils_plot.plot_results(results, num_random_exp=num_random_exp, figsize=(10, 5), show=False)
+    fig = utils_plot.plot_results(
+        results, num_random_exp=num_random_exp, figsize=(10, 5), show=False
+    )
     fig.axes[0].axhline(0.5, color="gray", alpha=0.8, linestyle="--")
     plt.savefig(results_dir / f"{target}_tcav_scores.png")
 
@@ -91,8 +101,11 @@ def main(args):
     for concept in concepts:
         accs = {}
         for bn in bottlenecks.keys():
-            accs[bn] = [v["cav_accuracies"]["overall"] for v in results if
-                        (v["bottleneck"] == bn) and (v["cav_concept"] == concept)]
+            accs[bn] = [
+                v["cav_accuracies"]["overall"]
+                for v in results
+                if (v["bottleneck"] == bn) and (v["cav_concept"] == concept)
+            ]
         accs = np.array(list(accs.values()))
         accs_mean = accs.mean(axis=1)
         accs_std = accs.std(axis=1)
@@ -105,37 +118,78 @@ def main(args):
         ax.plot(list(bottlenecks.keys()), all_acc_means[concept], label=concept)
     ax.set_xlabel("Layer")
     ax.set_ylabel("Accuracy")
-    ax.tick_params(axis='x', rotation=45)
+    ax.tick_params(axis="x", rotation=45)
     plt.legend(frameon=False)
     plt.savefig(results_dir / f"cav_accuracies.png")
 
-    print('Done!')
+    print("Done!")
 
 
 def create_model(dataset="imagenet"):
     if dataset == "imagenet":
         return models.resnet50(pretrained=True)
 
+
 def create_source_json(target, concepts, num_random_exp, data_path):
     source_json = {}
-    for concept in [target] + concepts + [f"random500_{i}" for i in range(num_random_exp)]:
+    for concept in (
+        [target] + concepts + [f"random500_{i}" for i in range(num_random_exp)]
+    ):
         paths = (data_path / concept).glob("*.jpg")
-        source_json[concept] = {i: {"path": f"{concept}/{path.name}", "label": 0} for i, path in enumerate(paths)}
+        source_json[concept] = {
+            i: {"path": f"{concept}/{path.name}", "label": 0}
+            for i, path in enumerate(paths)
+        }
     return source_json
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("--working-dir", help="Where to save intermediate outputs", default="/tmp/tcav/example")
+    parser.add_argument(
+        "--working-dir",
+        help="Where to save intermediate outputs",
+        default="/tmp/tcav/example",
+    )
     parser.add_argument("--source-dir", help="The location of the images")
-    parser.add_argument("--layers", help="A comma seperated list of the layers to run TCAV on",
-                        default="layer4.0, layer4.1")
-    parser.add_argument("--num-rand", help="The number of random experiments to run", type=int, default=30)
+    parser.add_argument(
+        "--layers",
+        help="A comma seperated list of the layers to run TCAV on",
+        default="layer4.0, layer4.1",
+    )
+    parser.add_argument(
+        "--num-rand",
+        help="The number of random experiments to run",
+        type=int,
+        default=30,
+    )
     parser.add_argument("--target", help="The target class name")
-    parser.add_argument("--concepts", help="A comma seperated list of the concepts you wish to create CAVs for")
-    parser.add_argument("--max-examples", help="The maximum number of images per concept", type=int, default=100)
-    parser.add_argument("--num-workers", help="The number of cpu workers for dataloading", type=int, default=4)
-    parser.add_argument("--exp-name", help="Experiment name (for saving results)", default="example")
-    parser.add_argument("--act-func", help="Optional function to apply to the model activations", default=None)
-    parser.add_argument("--overwrite", help="Overwrite activations, CAVs and gradients", action="store_true")
+    parser.add_argument(
+        "--concepts",
+        help="A comma seperated list of the concepts you wish to create CAVs for",
+    )
+    parser.add_argument(
+        "--max-examples",
+        help="The maximum number of images per concept",
+        type=int,
+        default=100,
+    )
+    parser.add_argument(
+        "--num-workers",
+        help="The number of cpu workers for dataloading",
+        type=int,
+        default=4,
+    )
+    parser.add_argument(
+        "--exp-name", help="Experiment name (for saving results)", default="example"
+    )
+    parser.add_argument(
+        "--act-func",
+        help="Optional function to apply to the model activations",
+        default=None,
+    )
+    parser.add_argument(
+        "--overwrite",
+        help="Overwrite activations, CAVs and gradients",
+        action="store_true",
+    )
     main(parser.parse_args())
--- visualise_tcav.py
+++ visualise_tcav.py
@@ -1,4 +1,3 @@
-
 from argparse import ArgumentParser
 from pathlib import Path
 import json
@@ -27,8 +26,8 @@ def main(args):
     act_func = args.act_func
 
     working_dir = Path(args.working_dir) / act_func
-    activation_dir = working_dir / 'activations'
-    cav_dir = working_dir / 'cavs'
+    activation_dir = working_dir / "activations"
+    cav_dir = working_dir / "cavs"
     grad_dir = working_dir / "grads"
 
     source_dir = Path(args.source_dir)  # '/home/lina3782/labs/explain/imagenet'
@@ -59,9 +58,16 @@ def main(args):
         source_json = json.load(fp)
 
     prefix = str(data_path) + "/"
-    act_generator = act_gen.ActivationGenerator(mymodel, source_json_path, activation_dir, JsonDataset,
-                                                max_examples=max_examples, prefix=prefix, num_workers=num_workers,
-                                                act_func=act_func)
+    act_generator = act_gen.ActivationGenerator(
+        mymodel,
+        source_json_path,
+        activation_dir,
+        JsonDataset,
+        max_examples=max_examples,
+        prefix=prefix,
+        num_workers=num_workers,
+        act_func=act_func,
+    )
 
     mytcav = tcav.TCAV(
         target,
@@ -72,7 +78,7 @@ def main(args):
         cav_dir=cav_dir,
         num_random_exp=num_random_exp,
         do_random_pairs=False,
-        grad_dir=grad_dir
+        grad_dir=grad_dir,
     )
 
     target_id = mymodel.label_to_id(target)
@@ -115,7 +121,11 @@ def main(args):
                 bn_grads.append(directional_derivatives)
 
                 fig, ax = rescale_and_plot(directional_derivatives, img)
-                plt.savefig(img_out_dir / concept / f"{img_path.stem}_{bn}_directional_derivative.png")
+                plt.savefig(
+                    img_out_dir
+                    / concept
+                    / f"{img_path.stem}_{bn}_directional_derivative.png"
+                )
                 plt.close(fig)
 
                 fig, ax = rescale_and_plot(act_dot, img)
@@ -127,9 +137,16 @@ def main(args):
         all_dot_acts.append(img_acts)
 
     bottlenecks = list(bottlenecks.values())
-    for values, name in zip([all_dot_acts, all_directional_derivatives], ["act_dot", "directional_derivative"]):
+    for values, name in zip(
+        [all_dot_acts, all_directional_derivatives],
+        ["act_dot", "directional_derivative"],
+    ):
         for c, concept in enumerate(concepts):
-            fig, axes = plt.subplots(len(bottlenecks), len(imgs), figsize=(2*len(imgs), 2*len(bottlenecks)))
+            fig, axes = plt.subplots(
+                len(bottlenecks),
+                len(imgs),
+                figsize=(2 * len(imgs), 2 * len(bottlenecks)),
+            )
             for i, row in enumerate(axes):
                 bn = bottlenecks[i]
                 all_vs = [values[j][bn][c] for j in range(len(row))]
@@ -137,14 +154,16 @@ def main(args):
                 for j, ax in enumerate(row):
                     ax.imshow(imgs[j])
                     act_rescaled = rescale_array(values[j][bn][c], imgs[j])
-                    s = ax.imshow(act_rescaled, alpha=0.4, cmap="seismic", vmin=-max_v, vmax=max_v)
+                    s = ax.imshow(
+                        act_rescaled, alpha=0.4, cmap="seismic", vmin=-max_v, vmax=max_v
+                    )
                     ax.axis("off")
                     if j == len(row) - 1:
                         fig.colorbar(s, ax=ax, fraction=0.046, pad=0.04)
             plt.tight_layout()
             plt.savefig(img_out_dir / concept / f"{name}_array.png")
 
-    print('Done!')
+    print("Done!")
 
 
 def load_img(path):
@@ -179,7 +198,7 @@ def cosine_sim(grad, cav):
     v = dot_product(grad, cav)
     norms = np.linalg.norm(grad[0], axis=0)
     cav_norm = np.linalg.norm(cav)
-    v = v / (norms*cav_norm)
+    v = v / (norms * cav_norm)
     return v
 
 
@@ -191,10 +210,12 @@ def get_cav(param):
 
     # Get CAVs
     cav_hparams = CAV.default_hparams()
-    cav_hparams['alpha'] = alpha
-    a_cav_key = CAV.cav_key(concepts, bottleneck, cav_hparams['model_type'], cav_hparams['alpha'])
+    cav_hparams["alpha"] = alpha
+    a_cav_key = CAV.cav_key(
+        concepts, bottleneck, cav_hparams["model_type"], cav_hparams["alpha"]
+    )
 
-    cav_path = cav_dir / (a_cav_key.replace('/', '.') + '.pkl')
+    cav_path = cav_dir / (a_cav_key.replace("/", ".") + ".pkl")
     cav_instance = CAV.load_cav(cav_path)
     cav_concept = concepts[0]
     direction = cav_instance.get_direction(cav_concept)
@@ -207,15 +228,46 @@ def create_model():
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("--working-dir", help="Where to save intermediate outputs", default="/tmp/tcav/example")
+    parser.add_argument(
+        "--working-dir",
+        help="Where to save intermediate outputs",
+        default="/tmp/tcav/example",
+    )
     parser.add_argument("--source-dir", help="The location of the images")
-    parser.add_argument("--layers", help="A comma seperated list of the layers to run TCAV on",
-                        default="layer4.0, layer4.1")
-    parser.add_argument("--num-rand", help="The number of random experiments to run", type=int, default=30)
+    parser.add_argument(
+        "--layers",
+        help="A comma seperated list of the layers to run TCAV on",
+        default="layer4.0, layer4.1",
+    )
+    parser.add_argument(
+        "--num-rand",
+        help="The number of random experiments to run",
+        type=int,
+        default=30,
+    )
     parser.add_argument("--target", help="The target class name")
-    parser.add_argument("--concepts", help="A comma seperated list of the concepts you wish to create CAVs for")
-    parser.add_argument("--max-examples", help="The maximum number of images per concept", type=int, default=5)
-    parser.add_argument("--num-workers", help="The number of cpu workers for dataloading", type=int, default=4)
-    parser.add_argument("--exp-name", help="Experiment name (for saving results)", default="example")
-    parser.add_argument("--act-func", help="Optional function to apply to the model activations", default=None)
+    parser.add_argument(
+        "--concepts",
+        help="A comma seperated list of the concepts you wish to create CAVs for",
+    )
+    parser.add_argument(
+        "--max-examples",
+        help="The maximum number of images per concept",
+        type=int,
+        default=5,
+    )
+    parser.add_argument(
+        "--num-workers",
+        help="The number of cpu workers for dataloading",
+        type=int,
+        default=4,
+    )
+    parser.add_argument(
+        "--exp-name", help="Experiment name (for saving results)", default="example"
+    )
+    parser.add_argument(
+        "--act-func",
+        help="Optional function to apply to the model activations",
+        default=None,
+    )
     main(parser.parse_args())
